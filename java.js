8a6438168fa2879269075d183f7d8a11dda7b522
lukaslmfdk <56252345+lukaslmfdk@users.noreply.github.com>
2021-09-15 09:32:16 +0200

    pop up commit

--- java.js
+++ java.js
@@ -88,7 +88,8 @@ function visOller() {
     popUp.querySelector(".duft_tekst_popUp").textContent = ol.duft;
     popUp.querySelector(".smagBeskrivelse_popUp").textContent = ol.smag;
     popUp.querySelector(".rating_popUp").textContent = ol.rating + "/10";
-
+    popUp.querySelector(".alkoholProcent_popUp").textContent =
+      ol.alkoholprocent + "%";
     document.querySelector("#luk_knap").addEventListener("click", skjulmenu);
 
     function skjulmenu() {
